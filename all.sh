fadbd3c89dd1b3b6bf55451026a5befb102c474f
Enosh Misonge Osano <osanoeneosh@gmail.com>
2026-02-16 20:46:44 +0300

    all.sh

--- all.sh
+++ all.sh
@@ -13,4 +13,4 @@ export PATH="$MISTY_PATH/bin:$PATH"
 #-------------------------------------------------------------------------------
 # 2. Begin modular installation
 #-------------------------------------------------------------------------------
-source "$MISTLETOE_INSTALL/config/all.sh"
+source "$MISTLETOE_INSTALL/all.sh"
