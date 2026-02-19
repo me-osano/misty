e5c05a9aacf888d3fc5bd0ab25bc33627f5cf14c
Enosh Misonge Osano <osanoeneosh@gmail.com>
2026-02-19 22:49:51 +0300

    Rofi

---
+++ themes/rosepine/emacs.el
@@ -0,0 +1,46 @@
+;; rose-pine-dawn-misty-palette.el
+(defconst misty-colors
+  '((rosewater . "#ea9d34")
+    (flamingo  . "#d7827e")
+    (pink      . "#b4637a")
+    (magenta   . "#907aa9")
+    (mauve     . "#907aa9")
+
+    (red       . "#b4637a")
+    (maroon    . "#d7827e")
+    (peach     . "#ea9d34")
+    (yellow    . "#ea9d34")
+    (green     . "#286983")
+    (orange    . "#ea9d34")
+
+    (teal      . "#56949f")
+    (sky       . "#56949f")
+    (sapphire  . "#286983")
+    (blue      . "#286983")
+    (lavender  . "#907aa9")
+
+    (text      . "#575279")
+    (const     . "#56949f")
+    (var       . "#575279")
+
+    (subtext1  . "#6e6a86")
+    (subtext0  . "#797593")
+
+    (overlay2  . "#9893a5")
+    (overlay1  . "#a89aa9")
+    (overlay0  . "#b8adb8")
+
+    (comment   . "#9893a5")
+
+    (surface2  . "#dfdad9")
+    (surface1  . "#f2ede9")
+    (surface0  . "#faf4ed")
+
+    (base      . "#faf4ed")
+    (base1     . "#fffbf8")
+
+    (warning   . "#ea9d34")
+
+    (mantle    . "#f6f1f0")
+    (crust     . "#f0ede8"))
+  "Rose Pine (Dawn) misty palette.")
